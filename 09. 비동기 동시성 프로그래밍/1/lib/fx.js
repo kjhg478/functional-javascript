94e0ff511cf5b6ed2e59e3210ba3676c75ee00d6
김종훈 <kjhg478@naver.com>
2022-01-09 21:29:16 +0900

    비동기 동시성 프로그래밍 비동기 제어와 Promise.then의 규칙

--- 09. 비동기 동시성 프로그래밍/1/lib/fx.js
+++ 09. 비동기 동시성 프로그래밍/1/lib/fx.js
@@ -1,11 +1,34 @@
 const log = console.log;
 
-const curry = f =>
-  (a, ..._) => _.length ? f(a, ..._) : (..._) => f(a, ..._);
+const curry =
+  (f) =>
+  (a, ..._) =>
+    _.length ? f(a, ..._) : (..._) => f(a, ..._);
 
-const isIterable = a => a && a[Symbol.iterator];
+const isIterable = (a) => a && a[Symbol.iterator];
 
-const go1 = (a, f) => a instanceof Promise ? a.then(f) : f(a);
+const go1 = (a, f) => (a instanceof Promise ? a.then(f) : f(a));
+
+/*
+  const reduce = curry((f, acc, iter) => {
+  if (!iter) {
+    iter = acc[Symbol.iterator]();
+    acc = iter.next().value;
+  } else {
+    iter = iter[Symbol.iterator]();
+  }
+  
+  let cur;
+  while (!(cur = iter.next()).done) {
+    const a = cur.value;
+    acc = f(acc, a);
+    acc = acc instanceof Promise ? acc.then(acc => f(acc, a)) : f(acc, a);
+    // 중간에 프로미스를 만나도 프로미스가 아닐 때는 동기적으로 다음으로 넘어갈 수 있게 수행
+  }
+  return acc;
+});
+
+*/
 
 const reduce = curry((f, acc, iter) => {
   if (!iter) {
@@ -14,6 +37,8 @@ const reduce = curry((f, acc, iter) => {
   } else {
     iter = iter[Symbol.iterator]();
   }
+  // 재귀를 이용해 위 코드를 보완
+  // acc가 프로미스면 풀어서 전달하고, 아니면 그냥 전달해서 하나의 콜스택으로 진행되도록 동기적으로 실행
   return go1(acc, function recur(acc) {
     let cur;
     while (!(cur = iter.next()).done) {
@@ -21,13 +46,17 @@ const reduce = curry((f, acc, iter) => {
       acc = f(acc, a);
       if (acc instanceof Promise) return acc.then(recur);
     }
+    // 시작할 때 go1이라는 함수를 즉시 시작하고, 동기적 코드면 if문에 들어오지 않고 실행을 안함
     return acc;
   });
 });
 
 const go = (...args) => reduce((a, f) => f(a), args);
 
-const pipe = (f, ...fs) => (...as) => go(f(...as), ...fs);
+const pipe =
+  (f, ...fs) =>
+  (...as) =>
+    go(f(...as), ...fs);
 
 const take = curry((l, iter) => {
   let res = [];
@@ -86,11 +115,7 @@ const map = curry(pipe(L.map, takeAll));
 
 const filter = curry(pipe(L.filter, takeAll));
 
-const find = curry((f, iter) => go(
-  iter,
-  L.filter(f),
-  take(1),
-  ([a]) => a));
+const find = curry((f, iter) => go(iter, L.filter(f), take(1), ([a]) => a));
 
 const flatten = pipe(L.flatten, takeAll);
 
@@ -98,7 +123,7 @@ const flatMap = curry(pipe(L.map, flatten));
 
 var add = (a, b) => a + b;
 
-const range = l => {
+const range = (l) => {
   let i = -1;
   let res = [];
   while (++i < l) {
